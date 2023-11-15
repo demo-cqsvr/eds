65cdc6afaf9671c8923110297842d24ec4889269
tekaneko <tekaneko@vm-mac.local>
2023-11-14 22:42:26 -0800

    update

--- blocks/slick/slick.js
+++ blocks/slick/slick.js
@@ -12,6 +12,8 @@ export default async function decorate(block) {
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
     <h1> Slick Block </h1>
+    <div class="slick-items">
     ${defaultHTML}
+    </div>
     `;      
 }
