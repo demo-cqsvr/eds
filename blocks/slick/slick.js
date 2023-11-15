5996b201f77732c4bc4436ac8ed570cba2cf5ff1
tekaneko <tekaneko@vm-mac.local>
2023-11-14 22:40:25 -0800

    slick

--- blocks/slick/slick.js
+++ blocks/slick/slick.js
@@ -7,9 +7,11 @@
 export default async function decorate(block) {
     const defaultHTML = block.innerHTML;
     block.innerHTML = `
-    <hr>
+    <link ref="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css/>
+    <link ref="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
     <h1> Slick Block </h1>
-    <h2>${defaultHTML}</h2>
-    <hr>
+    ${defaultHTML}
     `;      
 }
